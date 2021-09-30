03c856f0721bd29241868db98f178093edf93334
Damerian Dark <newbmailz@gmail.com>
2021-09-30 15:01:42 -0500

    service worker working

--- public/service-worker.js
+++ public/service-worker.js
@@ -4,19 +4,16 @@ const FILES_TO_CACHE = [
   "/",
   "/index.html",
   "/manifest.webmanifest",
-  "icons/icon-192x192.png",
+  "/icons/icon-192x192.png",
   "/icons/icon-512x512.png",
   "/db.js",
-  "/stles.css"
- 
+  "/styles.css",
+  "/index.js"
 ];
 
 // install
 self.addEventListener("install", function (evt) {
-  // pre cache image data
-  evt.waitUntil(
-    caches.open(DATA_CACHE_NAME).then((cache) => cache.add("/api/images"))
-  );
+
     
   // pre cache all static assets
   evt.waitUntil(
